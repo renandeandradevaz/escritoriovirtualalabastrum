d9ec81b296ac7201ee1c0b41bc5c7405000f3f53
Renan Vaz <renan.vaz@bemobi.com.br>
2018-07-07 22:58:01 -0300

    -

---
+++ src/br/com/alabastrum/escritoriovirtual/controller/DistribuidorController.java
@@ -0,0 +1,34 @@
+package br.com.alabastrum.escritoriovirtual.controller;
+
+import static br.com.caelum.vraptor.view.Results.json;
+
+import br.com.alabastrum.escritoriovirtual.anotacoes.Public;
+import br.com.alabastrum.escritoriovirtual.hibernate.HibernateUtil;
+import br.com.alabastrum.escritoriovirtual.modelo.Usuario;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+@Resource
+public class DistribuidorController {
+
+	private Result result;
+	private HibernateUtil hibernateUtil;
+
+	public DistribuidorController(Result result, HibernateUtil hibernateUtil) {
+		this.result = result;
+		this.hibernateUtil = hibernateUtil;
+	}
+
+	@Public
+	public void obterNomeDistribuidor(Integer codigo) {
+
+		Usuario usuario = hibernateUtil.selecionar(new Usuario(codigo));
+
+		if (usuario == null) {
+			result.use(json()).from("").serialize();
+
+		} else {
+			result.use(json()).from(usuario.getvNome()).serialize();
+		}
+	}
+}
