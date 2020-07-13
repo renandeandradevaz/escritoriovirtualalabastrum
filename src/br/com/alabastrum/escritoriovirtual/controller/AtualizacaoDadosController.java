e3973a839bc2c23c5f3d6af0ec16471b1e01757d
Renan Vaz <renan.vaz@bemobi.com.br>
2020-07-13 15:02:50 -0300

    nova tela de cadastro

--- src/br/com/alabastrum/escritoriovirtual/controller/AtualizacaoDadosController.java
+++ src/br/com/alabastrum/escritoriovirtual/controller/AtualizacaoDadosController.java
@@ -90,10 +90,8 @@ public class AtualizacaoDadosController {
 		textoArquivo += "Codigo: \'" + usuario.getCodigoQuemIndicou() + "\'\r\n";
 		textoArquivo += "nomepatroc: \'" + usuario.getNomeQuemIndicou() + "\'\r\n";
 		
-		System.out.println(textoArquivo);
-
 		ArquivoService.criarArquivoNoDisco(textoArquivo, ArquivoService.PASTA_PRE_CADASTRO);
-		//Mail.enviarEmail("Pré cadastro pelo site", textoArquivo);
+		Mail.enviarEmail("Pré cadastro pelo site", textoArquivo);
 
 		result.redirectTo("https://dunastes.com.br/sucesso");
 	}
