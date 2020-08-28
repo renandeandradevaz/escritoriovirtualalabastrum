89c8306192ec43635c50d74c1c01c49695ce595b
Renan Vaz <renan.vaz@bemobi.com.br>
2020-08-28 17:02:37 -0300

    pontuacao

--- src/br/com/alabastrum/escritoriovirtual/service/PontuacaoService.java
+++ src/br/com/alabastrum/escritoriovirtual/service/PontuacaoService.java
@@ -208,7 +208,7 @@ public class PontuacaoService {
 	if (graduacaoMensalDTO.getPontosFeitosAteOMomento().equals(0)) {
 	    graduacaoMensalDTO.setPorcentagemConclusao(0);
 	} else {
-	    graduacaoMensalDTO.setPorcentagemConclusao(new BigDecimal(graduacaoMensalDTO.getPontosFeitosAteOMomento()).divide(new BigDecimal(graduacaoMensalDTO.getPontuacaoDaProximaPosicao()).multiply(new BigDecimal(100)), 2, RoundingMode.HALF_UP).intValue());
+	    graduacaoMensalDTO.setPorcentagemConclusao(new BigDecimal(graduacaoMensalDTO.getPontosFeitosAteOMomento()).divide(new BigDecimal(graduacaoMensalDTO.getPontuacaoDaProximaPosicao()), 2, RoundingMode.HALF_UP).multiply(new BigDecimal(100)).intValue());
 	}
 
 	return graduacaoMensalDTO;
