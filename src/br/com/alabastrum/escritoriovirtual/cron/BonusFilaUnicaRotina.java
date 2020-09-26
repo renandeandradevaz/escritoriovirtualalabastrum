fc07767525d155c00d336e325810708fbedd696d
Renan Vaz <renan.vaz@bemobi.com.br>
2020-09-26 18:29:53 -0300

    comecando bonus fila unica

--- src/br/com/alabastrum/escritoriovirtual/cron/BonusFilaUnicaRotina.java
+++ src/br/com/alabastrum/escritoriovirtual/cron/BonusFilaUnicaRotina.java
@@ -30,36 +30,54 @@ public class BonusFilaUnicaRotina implements Runnable {
 	    GregorianCalendar primeiroDiaDoMes = Util.getPrimeiroDiaDoMes(ontem);
 	    GregorianCalendar ultimoDiaDoMes = Util.getUltimoDiaDoMes(ontem);
 
-	    Integer pontuacaoProdutoCompletaDoMes = new PontuacaoService(hibernateUtil).calcularPontuacaoDeProdutoTodaEmpresa(primeiroDiaDoMes, ultimoDiaDoMes);
-	    Integer pontuacaoFilaUnicaDoMes = new PontuacaoService(hibernateUtil).calcularPontuacaoFilaUnicaTodaEmpresa(primeiroDiaDoMes, ultimoDiaDoMes);
-	    Integer pontuacaoTotalNoMes = pontuacaoProdutoCompletaDoMes + pontuacaoFilaUnicaDoMes;
+	    List<Usuario> usuariosHabilitados = buscarUsuariosHabilitados(hibernateUtil, ontem);
+	    int quantidadeUsuariosHabilitados = usuariosHabilitados.size();
 
-	    BigDecimal valorASerDivididoNoMes = new BigDecimal(pontuacaoTotalNoMes).multiply(new BigDecimal(0.12));
+	    if (quantidadeUsuariosHabilitados > 0) {
 
-	    List<Usuario> usuariosHabilitados = new ArrayList<Usuario>();
+		Integer pontuacaoProdutoCompletaDoMes = new PontuacaoService(hibernateUtil).calcularPontuacaoDeProdutoTodaEmpresa(primeiroDiaDoMes, ultimoDiaDoMes);
+		Integer pontuacaoFilaUnicaDoMes = new PontuacaoService(hibernateUtil).calcularPontuacaoFilaUnicaTodaEmpresa(primeiroDiaDoMes, ultimoDiaDoMes);
+		Integer pontuacaoTotalNoMes = pontuacaoProdutoCompletaDoMes + pontuacaoFilaUnicaDoMes;
+		BigDecimal valorASerDivididoNoMes = new BigDecimal(pontuacaoTotalNoMes).multiply(new BigDecimal(0.12));
+		BigDecimal valorCota = valorASerDivididoNoMes.divide(new BigDecimal(quantidadeUsuariosHabilitados), 2, BigDecimal.ROUND_HALF_UP).divide(new BigDecimal(quantidadeUsuariosHabilitados / 2 + 0.5), 2, BigDecimal.ROUND_HALF_UP);
 
-	    List<Usuario> usuarios = hibernateUtil.buscar(new Usuario());
-	    for (Usuario usuario : usuarios) {
-		if (new AtividadeService(hibernateUtil).isAtivo(usuario.getId_Codigo(), ontem) && new AtividadeService(hibernateUtil).possuiIndicadosDiretosAtivos(usuario.getId_Codigo(), ontem, 3)) {
-		    GraduacaoMensalDTO graduacaoMensal = new PontuacaoService(hibernateUtil).calcularGraduacaoMensal(usuario.getId_Codigo(), ontem);
-		    if (graduacaoMensal.getPontosAproveitados() >= 2000) {
-			usuariosHabilitados.add(usuario);
-		    }
+		for (int i = 0; i < usuariosHabilitados.size(); i++) {
+
+		    Usuario usuarioHabilitado = usuariosHabilitados.get(i);
+		    int quantidadeDeCotasDoUsuario = quantidadeUsuariosHabilitados - i;
+
+		    BigDecimal valorParaPagamento = valorCota.multiply(new BigDecimal(quantidadeDeCotasDoUsuario));
+
 		}
 	    }
-	    Collections.sort(usuariosHabilitados, new Comparator<Usuario>() {
-		public int compare(Usuario usuario1, Usuario usuario2) {
-		    return usuario1.getFila_unica().compareTo(usuario2.getFila_unica());
-		}
-	    });
-	    
-	    int quantidadeUsuariosHbili
 
 	} catch (Exception e) {
 	    String errorString = Util.getExceptionMessage(e);
 	    Mail.enviarEmail("Exception ao rodar rotina de bonus de fila unica", errorString);
 	}
 	hibernateUtil.fecharSessao();
+    }
+
+    private List<Usuario> buscarUsuariosHabilitados(HibernateUtil hibernateUtil, GregorianCalendar ontem) {
+
+	List<Usuario> usuariosHabilitados = new ArrayList<Usuario>();
+
+	List<Usuario> usuarios = hibernateUtil.buscar(new Usuario());
+	for (Usuario usuario : usuarios) {
+	    if (new AtividadeService(hibernateUtil).isAtivo(usuario.getId_Codigo(), ontem) && new AtividadeService(hibernateUtil).possuiIndicadosDiretosAtivos(usuario.getId_Codigo(), ontem, 3)) {
+		GraduacaoMensalDTO graduacaoMensal = new PontuacaoService(hibernateUtil).calcularGraduacaoMensal(usuario.getId_Codigo(), ontem);
+		if (graduacaoMensal.getPontosAproveitados() >= 2000) {
+		    usuariosHabilitados.add(usuario);
+		}
+	    }
+	}
+	Collections.sort(usuariosHabilitados, new Comparator<Usuario>() {
+	    public int compare(Usuario usuario1, Usuario usuario2) {
+		return usuario1.getFila_unica().compareTo(usuario2.getFila_unica());
+	    }
+	});
+
+	return usuariosHabilitados;
     }
 
     public void iniciarRotina() {
