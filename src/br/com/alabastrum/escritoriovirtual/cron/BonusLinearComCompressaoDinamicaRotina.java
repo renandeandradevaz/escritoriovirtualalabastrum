a1c19c1cf6984054232008861dd2effa4c140a35
Renan Vaz <renanandrade_rj@hotmail.com>
2020-10-20 10:47:18 -0300

    Update BonusLinearComCompressaoDinamicaRotina.java

--- src/br/com/alabastrum/escritoriovirtual/cron/BonusLinearComCompressaoDinamicaRotina.java
+++ src/br/com/alabastrum/escritoriovirtual/cron/BonusLinearComCompressaoDinamicaRotina.java
@@ -33,8 +33,8 @@ public class BonusLinearComCompressaoDinamicaRotina implements Runnable {
 	    List<Usuario> usuarios = hibernateUtil.buscar(new Usuario());
 
 	    for (Usuario usuario : usuarios) {
-
-		List<Pontuacao> pontuacoes = new PontuacaoService(hibernateUtil).buscarPontuacoes(usuario.getId_Codigo());
+	
+		List<Pontuacao> pontuacoes = new PontuacaoService(hibernateUtil).buscarPontuacoes(usuario.getId_Codigo(), primeiroDiaDoMes, ultimoDiaDoMes);
 
 		if (Util.preenchido(pontuacoes)) {
 
